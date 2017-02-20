f7e0409af2826492fbf8dd6b515f72be5b48f687
dusan <rostacik@gmail.com>
2017-02-20 11:42:23 +0100

    added nice formatting for ya console

--- beervana-cli/index.js
+++ beervana-cli/index.js
@@ -1,5 +1,22 @@
-var scb = require("beervana-scraper");
+var scb = require('beervana-scraper');
+
+/**
+ * yeeee, format that object, yeeee, like that * 
+ * @param {beer} beer 
+ * @returns {string} nice result
+ */
+function objToString(obj) {
+    var str = '';
+    for (var p in obj) {
+        if (obj.hasOwnProperty(p)) {
+            str += p + '::' + obj[p] + '\n';
+        }
+    }
+    return str;
+}
 
 scb().then(page => {
-    console.log(page.beer);
+    for (let beer of page.beer) {
+        console.log(objToString(beer));
+    }
 });
