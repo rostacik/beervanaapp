d47f3fd14a1a8e713a4a3a220d7d215173cb01f4
Dusan Rostar <rostacik@gmail.com>
2017-02-26 17:37:04 +0100

    fixed serializing

--- beervana-cli/index.js
+++ beervana-cli/index.js
@@ -2,14 +2,14 @@ var scb = require('beervana-scraper');
 
 /**
  * yeeee, format that object, yeeee, like that * 
- * @param {beer} beer 
+ * @param {beer} beer object to be serialized
  * @returns {string} nice result
  */
 function objToString(obj) {
     var str = '';
     for (var p in obj) {
         if (obj.hasOwnProperty(p)) {
-            str += p + '::' + obj[p] + '\n';
+            str += p + ' : ' + obj[p] + '\n';
         }
     }
     return str;
